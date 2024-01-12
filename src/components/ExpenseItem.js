23e738bcb90c6f8327d2a276ec1f53b87f4559a6
Dudeyay <xuzhexihk@gmail.com>
2024-01-12 06:56:05 -0500

    Created shopping app that calculates Cart Value, allows user to select location, and add various items with varying quantities into the cart.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,24 @@ import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
+    const {dispatch,Location} = useContext(AppContext);
+    const handleDeleteItem = ()=>{
+        const action = {
+            type: 'DELETE_ITEM',
+            payload: {name: props.name}
+        }
+        dispatch(action);
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.quantity}</td>
+            <td>{Location}{props.unitprice}</td>
+            <td>{Location}{parseInt(props.quantity) * parseInt(props.unitprice)}</td>
+            <td><td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td></td>
+        </tr>
+    );
 };
 
 export default ExpenseItem;
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+    const {dispatch,Location} = useContext(AppContext);
+    const [location, setLocation] = useState(Location);
+    const action = {
+        type: 'CHG_LOCATION',
+        payload: location
+    }
+    const submitEvent = (event) => {
+        setLocation(event.target.value);
+        dispatch(action);
+    }
+    return (
+        <div className='alert alert-secondary'> Location {
+            <select name="Location" id="Location" onChange={submitEvent}>
+                <option value="£">Uk(£)</option>
+        <option value="₹">India(₹)</option>
+        <option value="€">Europe(€)</option>
+        <option value="CAD">Canada(CAD)</option>
+            </select> 
+        }
+        </div>
+
+    );
 };
 
 export default Location;
